8e9a2b908a92a8ee010f084bae75308b0e979000
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2025-05-30 04:54:16 +0300

    fix typo

--- src/main/java/com/mykola/railroad/dto/BrigadeDTO.java
+++ src/main/java/com/mykola/railroad/dto/BrigadeDTO.java
@@ -3,7 +3,7 @@ package com.mykola.railroad.dto;
 import lombok.Data;
 
 @Data
-public class BridadeDTO {
+public class BrigadeDTO {
     public Integer id;
     public Integer department;
     public Integer brigadier;
